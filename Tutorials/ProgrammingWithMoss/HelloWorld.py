9061c082ce714e5ee7b4a80be21e60927602ed68
Ben Reinert <146287616+Soccertoad@users.noreply.github.com>
2024-06-10 14:30:29 -0400

    Print statements

--- Tutorials/ProgrammingWithMoss/HelloWorld.py
+++ Tutorials/ProgrammingWithMoss/HelloWorld.py
@@ -1,1 +1,21 @@
+# Comments
+# Theory
+#   Python runs line by line
+#   
+
+# What is it
+#   Strings: Things inside quotations
+#   Integers: Numbers not in quotations
+#   Math: + adds; - subtracts; * multiplies; / divides;
+#   Expressions: a piece of code that produces a value
+
+#       Prints Hello World to terminal
 print("Hello World")
+
+#       Prints a dog to terminal
+print("o----")
+print(" ||||")
+
+#       Expression \/
+#       Prints 10 *'s
+print('*' * 10)
