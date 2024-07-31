c6171c3cf5e8afd831aa8954a2dbb9aa6c931b94
Ben Reinert <146287616+Soccertoad@users.noreply.github.com>
2024-07-30 21:00:43 -0400

    turtle

---
+++ Tutorials/Python-For-Kids/turtorls.py
@@ -0,0 +1,23 @@
+import turtle
+
+window = turtle.Screen()
+t = turtle.Pen()
+
+t.forward(50)
+t.left(90)
+
+t.reset()
+t.backward(100)
+t.up()
+t.right(90)
+t.forward(20)
+t.left(90)
+t.down()
+t.forward(100)
+
+#Make Rectangle
+#make triangle
+#make box without corners
+
+window.exitonclick()
+turtle.done()
